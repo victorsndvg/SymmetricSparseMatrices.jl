e1ed54d0e5699237c81f885e29a3a4bec3b0e3fb
victorsndvg <victorsv@gmail.com>
2019-09-19 16:31:06 +0200

    Fix convertCOOrowstoCSR algorithms.
    Add some conversion tests

--- src/SymmetricSparseMatrices.jl
+++ src/SymmetricSparseMatrices.jl
@@ -15,15 +15,15 @@ Extension from two-dimensional arrays (or array-like types) with
 elements of type `T` and `I`. Alias for [`AbstractArray{T,2}`](@ref).
 """
     struct SymmetricSparseMatrix{T<:Real, I<:Integer} <: AbstractMatrix{T}
-        m      ::Int                # Number of rows
-        n      ::Int                # Number of columns
+        m      ::Int                # Number of rows/columns
         rowptr ::Vector{I}          # Row i is in rowptr[i]:(rowptr[i+1]-1) 
         colval ::Vector{I}          # Col indices of stored values
         nzval  ::Vector{T}          # Stored values, typically nonzeros
 
-        function SymmetricSparseMatrix{T,I}(m::Integer, n::Integer, rowptr::Vector{I}, colval::Vector{I}, nzval::Vector{T}) where {T<:Real,I<:Integer}
+        function SymmetricSparseMatrix{T,I}(m::Integer, rowptr::Vector{I}, colval::Vector{I}, nzval::Vector{T}) where {T<:Real,I<:Integer}
             rowptr[end]-1 == length(nzval) == length(colval) || throw(ArgumentError(string("Length of colval and nzval arrays does not match with rowptr[end]-1 ", "(",rowptr[end]-1,")")))
-            new(Int(m), Int(n), rowptr, colval, nzval)
+            rowptr[end]-1 == length(nzval) == length(colval) || throw(ArgumentError(string("Length of colval and nzval arrays does not match with rowptr[end]-1 ", "(",rowptr[end]-1,")")))
+            new(Int(m), rowptr, colval, nzval)
         end
     end # SymmetricSparseMatrix
 
@@ -32,39 +32,46 @@ elements of type `T` and `I`. Alias for [`AbstractArray{T,2}`](@ref).
     SymmetricSparseMatrix(m::Integer, n::Integer, rowptr::Vector, colval::Vector, nzval::Vector)
 SymetricSparseMatrix outter constructor method with full signature.
 """
-    function SymmetricSparseMatrix(m::Integer, n::Integer, rowptr::Vector, colval::Vector, nzval::Vector)
+    function SymmetricSparseMatrix(m::Integer, rowptr::Vector, colval::Vector, nzval::Vector)
         Tv = eltype(nzval)
         Ti = promote_type(eltype(rowptr), eltype(colval))
-        SymmetricSparseMatrix{Tv,Ti}(m, n, rowptr, colval, nzval)
+        SymmetricSparseMatrix{Tv,Ti}(m, rowptr, colval, nzval)
     end
 
 """
     SymmetricSparseMatrix()
 SymetricSparseMatrix outter constructor method to create empty .
 """
-    SymmetricSparseMatrix() = SymmetricSparseMatrix(0,0,[1],Vector{Int}(),Vector{Int}())
+    SymmetricSparseMatrix() = SymmetricSparseMatrix(0,[1],Vector{Int}(),Vector{Int}())
 
 
 """
     convertCOOrowstoCSR(m, I)
 Convert ordered COO rows (I) to CSR rowptr given its number of rows.
 """
-    function convertCOOrowstoCSR(m, I)
+    function convertCOOrowstoCSR(m, nnz, I)
         rowptr=zeros(eltype(I), m+1)
-        previ=1
-        counter=0
-        j=1
         rowptr[1]=1
-        for i in I
-            counter+=1
-            if i!=previ
+
+        if length(I) > 0
+            i = 1
+            j = 1
+            irw = I[j]
+
+            while i<=m
+                while i<irw; rowptr[i+1] = rowptr[i]; i+=1; end
+
                 j+=1
-                rowptr[j] = counter
-                counter+=0
-                previ = i
+                j>=nnz && break
+                if I[j] != irw
+                    rowptr[i+1] = j
+                    irw = I[j]
+                    i+=1
+                end
             end
+            while i <= m; rowptr[i+1] = j; i+=1; end
         end
-        rowptr[m+1]=counter+1
+        rowptr[m+1]=nnz+1
         return rowptr
     end
 
@@ -73,17 +80,18 @@ Convert ordered COO rows (I) to CSR rowptr given its number of rows.
 Convert SparseMatrixCSC{T,Ti} to SymmetricSparseMatrix.
 """
     function symsparse(S::SparseMatrixCSC{T,Ti}) where {T, Ti}
-        m = S.m                     # Number of rows
-        n = S.n                     # Number of columns
+        S.m == S.n || throw(ArgumentError(string("Only square input matrix are supported")))
+        m  = S.m                     # Number of rows
+        nz = nnz(S)                  # Number of columns
 
-        for i in 1:min(m,n) setindex!(S, getindex(S, i, i), i, i) end
+        for i in 1:m setindex!(S, getindex(S, i, i), i, i) end
 
         # Sort by rows
         I,J,K = findnz(S)
         permutation = sortperm(I)
 
         # Return SymmetricSparseMatrix CSR
-        return SymmetricSparseMatrix(m, n, convertCOOrowstoCSR(m, I[permutation]), J[permutation], K[permutation])
+        return SymmetricSparseMatrix(m, convertCOOrowstoCSR(m, nz, I[permutation]), J[permutation], K[permutation])
     end
 
 """
@@ -104,7 +112,7 @@ SymetriSparseMatrix show method implementation.
     size(A::SymmetricSparseMatrix)
 Returns the size (#rows, #columns) of the SymmetricSparseMatrix.
 """
-    size(A::SymmetricSparseMatrix) = (A.m, A.n)
+    size(A::SymmetricSparseMatrix) = (A.m, A.m)
 
 """
     nnz(A::SymmetricSparseMatrix)
@@ -117,7 +125,7 @@ Returns the number of stored (filled) elements in a SymmetricSparseMatrix.
 Return the (x, y) value of the SymmetricSparseMatrix.
 """
     function getindex(A::SymmetricSparseMatrix{T,I}, x::Integer, y::Integer) where {T, I}
-        if !(x in 1:A.m) || !(y in 1:A.n) throw(BoundsError(string(size(A), " SymmetricSparseMatrix at index ", "[",x,",", y,"]"))) end
+        if !(x in 1:A.m) || !(y in 1:A.m) throw(BoundsError(string(size(A), " SymmetricSparseMatrix at index ", "[",x,",", y,"]"))) end
         x_= min(x,y)
         y_= max(x,y)
         lbound=A.rowptr[x_]
@@ -135,7 +143,7 @@ Return the (x, y) value of the SymmetricSparseMatrix.
 Return the A*v product where `A` is SymmetricSparseMatrix{T,I} and `v` a Vector{T}.
 """
     function *(A::SymmetricSparseMatrix, v::Vector{T}) where {T<:Real}
-        b = size(v) == (A.m ,) ? zero(v) : throw(ArgumentError(string("v::Vector{T} size must match with SymmetricSparseMatrix number of rows", "(",A.m,")")))
+        b = length(v) == A.m ? zero(v) : throw(ArgumentError(string("v::Vector{T} size must match with SymmetricSparseMatrix number of rows", "(",A.m,")")))
         for x in 1:A.m
             lbound=A.rowptr[x]
             ubound=A.rowptr[x+1]-1
--- test/SymmetricSparseMatrix/runtests.jl
+++ test/SymmetricSparseMatrix/runtests.jl
@@ -1,10 +1,23 @@
 using SymmetricSparseMatrices
+using SparseArrays
 using Test
 
 @testset "SymmetricSparseMatrix" begin
-    @test size(SymmetricSparseMatrices.SymmetricSparseMatrix()) == (0,0)
-    @test nnz(SymmetricSparseMatrices.SymmetricSparseMatrix()) == 0
-    @test getindex(SymmetricSparseMatrices.SymmetricSparseMatrix(1,1,[1,2],[1],[1]),1,1) == 1
-    @test nnz(SymmetricSparseMatrices.SymmetricSparseMatrix(1,1,[1,2],[1],[1])) == 1
-    @test SymmetricSparseMatrices.SymmetricSparseMatrix(5,5,[1,4,6,9,12,14], [1,2,4,1,2,3,4,5,1,3,4,2,5], [1,-1,-3,-2,5,4,6,4,-4,2,7,8,-5])*ones(5) == [-3, 4, 14, 10, -1]
+    # SymmetricSparseMatrix
+    @test size(SymmetricSparseMatrix()) == (0,0)
+    @test nnz(SymmetricSparseMatrix()) == 0
+    @test getindex(SymmetricSparseMatrix(1,[1,2],[1],[1]),1,1) == 1
+    @test nnz(SymmetricSparseMatrix(1,[1,2],[1],[1])) == 1
+    @test SymmetricSparseMatrix(5,[1,4,6,9,12,14], [1,2,4,1,2,3,4,5,1,3,4,2,5], [1,-1,-3,-2,5,4,6,4,-4,2,7,8,-5])*ones(5) == [-3, 4, 14, 10, -1]
+
+    # Conversions
+    a = sparse([1,2,3,4],[1,2,3,4],[1,2,3,4])
+    b = Matrix(a)
+    c = symsparse(b)
+    @test a == b == c
+
+    a = sparse([1,2,3,4,1,2,3,4],[1,2,3,4,4,3,2,1],[1,2,3,4,4,3,2,1])
+    b = Matrix(a)
+    c = symsparse(b)
+    @ test a != c != b
 end
